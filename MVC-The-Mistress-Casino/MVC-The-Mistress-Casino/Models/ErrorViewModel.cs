a503b88637253a48f4ab6e21db865b87bf4f3775
ChrisTheDaneNerd <christianhoeirup@gmail.com>
2024-03-07 12:48:51 +0100

    Upload af alt

---
+++ MVC-The-Mistress-Casino/MVC-The-Mistress-Casino/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MVC_The_Mistress_Casino.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
